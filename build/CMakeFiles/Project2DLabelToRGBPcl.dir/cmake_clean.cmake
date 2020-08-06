4488cce24e86cb3aba2e9eee37fb77073918c293
jediofgever <fetulahatas1@gmail.com>
2020-08-06 18:03:19 +0800

    first commit

---
+++ build/CMakeFiles/Project2DLabelToRGBPcl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project2DLabelToRGBPcl.dir/src/Project2DLabelToRGBPcl.cpp.o"
+  "Project2DLabelToRGBPcl"
+  "Project2DLabelToRGBPcl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project2DLabelToRGBPcl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
